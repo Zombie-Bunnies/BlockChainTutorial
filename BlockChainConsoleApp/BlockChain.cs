0dbbb7aad6e41f9a8105aedd3dd1c11a82bd4d7a
David <David@DESKTOP-QFURPFT>
2021-11-23 20:39:06 +0700

    update for proof of work

--- BlockChainConsoleApp/BlockChain.cs
+++ BlockChainConsoleApp/BlockChain.cs
@@ -9,6 +9,7 @@ namespace BlockChainConsoleApp
     public class BlockChain
     {
         public IList<Block> Chain { get; set; }
+        public int Difficulty { get; set; } = 3;
 
         public BlockChain()
         {
@@ -17,14 +18,14 @@ namespace BlockChainConsoleApp
 
         }
 
-        public void AddGenesisBlock()
-        {
-            Chain.Add(CreateGenesisBlock());
-        }
-
         public Block CreateGenesisBlock()
         {
             return new Block(DateTime.Now, null, "{}");
+        }
+
+        public void AddGenesisBlock()
+        {
+            Chain.Add(CreateGenesisBlock());
         }
 
         public void InitializeChain()
@@ -42,8 +43,29 @@ namespace BlockChainConsoleApp
             Block latestBlock = GetLatestBlock();
             block.Index = latestBlock.Index + 1;
             block.PreviousHash = latestBlock.Hash;
-            block.Hash  = block.CalculateHash();
+            block.Hash = block.CalculateHash();
+            block.Mine(this.Difficulty);
             Chain.Add(block);
+        }
+
+        public bool IsValid()
+        {
+            for(int i = 1; i < Chain.Count; i++)
+            {
+                Block currentBlock = Chain[i];
+                Block previousBlock = Chain[i - 1];
+
+                if (currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+
+                if (currentBlock.PreviousHash != previousBlock.Hash)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
--- BlockChainConsoleApp/Program.cs
+++ BlockChainConsoleApp/Program.cs
@@ -11,12 +11,18 @@ namespace BlockChainConsoleApp
     {
         static void Main(string[] args)
         {
+            var startTime = DateTime.Now;
+
             BlockChain phillyCoin = new BlockChain();
             phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Henry,receiver:MaHesh,amount:10}"));
             phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:MaHesh,receiver:Henry,amount:5}}"));
             phillyCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Mahesh,receiver:Henry,amount:5}"));
 
+            var endTime = DateTime.Now;
+
             Console.WriteLine(JsonConvert.SerializeObject(phillyCoin, Formatting.Indented));
+            Console.WriteLine($"Is Chain Valid: {phillyCoin.IsValid()}");
+            Console.WriteLine($"Duration: {endTime - startTime}");
             Console.Read();
         }
     }
