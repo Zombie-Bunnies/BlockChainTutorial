9e79247ea9c54c95fb8b9a1e6eeba5781548fa7b
Zombie Bunny <94969304+Zombie-Bunnies@users.noreply.github.com>
2021-11-25 20:16:41 +0700

    unit test checking for nonce

--- BlockChainTutorial/UnitTest1.cs
+++ BlockChainTutorial/UnitTest1.cs
@@ -3,6 +3,8 @@ using System;
 using BlockChainConsoleApp;
 using System.Security.Policy;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace BlockChainTutorial
 {
@@ -57,6 +59,44 @@ namespace BlockChainTutorial
 
             //Assert
             Assert.AreEqual(genesisHashCode, nextHashCode);
+        }
+
+        [TestMethod]
+        public void ShouldCreateHashOfABC_ABD()
+        {
+            int nonce = 0;
+
+            SHA256 sHA256 = SHA256.Create();
+            byte[] inputBytes_ABC = Encoding.ASCII.GetBytes("ABC");
+            byte[] outputBytes_ABC = sHA256.ComputeHash(inputBytes_ABC);
+
+            //byte[] inputBytes_ABD = Encoding.ASCII.GetBytes("ABD");
+            //byte[] outputBytes_ABD = sHA256.ComputeHash(inputBytes_ABD);
+
+            //string ABC_Hash = Convert.ToBase64String(outputBytes_ABC);
+
+            //string ABD_Hash = Convert.ToBase64String(outputBytes_ABD);
+
+            for (int i = 0; i < 200000; i++)
+            {
+                byte[] inputBytes_ABC_PlusNonce = Encoding.ASCII.GetBytes($"{inputBytes_ABC}-{nonce}");
+                byte[] outputBytes_ABC_PlusNonce = sHA256.ComputeHash(inputBytes_ABC_PlusNonce);
+
+                string ABC_PlusNonce = Convert.ToBase64String(outputBytes_ABC_PlusNonce);
+
+                //bool originalHash = ABC_Hash.StartsWith("t");
+
+                bool checkABC_For_0 = ABC_PlusNonce.StartsWith("000");
+
+                if (checkABC_For_0)
+                {
+                    string message = "nonce is " + nonce;
+                    // winner winner chicken dinner
+                    Assert.IsFalse(checkABC_For_0, message);
+                }
+                nonce++;
+            }
+
         }
     }
 }
