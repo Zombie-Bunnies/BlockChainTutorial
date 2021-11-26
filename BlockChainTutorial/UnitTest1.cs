d425efaab794a92325763eed435954a09794fb20
Zombie Bunny <94969304+Zombie-Bunnies@users.noreply.github.com>
2021-11-26 15:42:24 +0700

    multiple nonce problem. find smallest nonce

--- BlockChainTutorial/UnitTest1.cs
+++ BlockChainTutorial/UnitTest1.cs
@@ -65,6 +65,8 @@ namespace BlockChainTutorial
         public void ShouldCreateHashOfABC_ABD()
         {
             int nonce = 0;
+            int difficulty = 3;
+            var leadingZeros = new string('0', difficulty);
 
             SHA256 sHA256 = SHA256.Create();
             byte[] inputBytes_ABC = Encoding.ASCII.GetBytes("ABC");
@@ -87,9 +89,9 @@ namespace BlockChainTutorial
 
                 //bool originalHash = ABC_Hash.StartsWith("t");
 
-                bool checkABC_For_0 = ABC_PlusNonce.StartsWith("000");
+                bool checkABC_For_leadingZeros = ABC_PlusNonce.StartsWith(leadingZeros);
 
-                if (checkABC_For_0)
+                if (checkABC_For_leadingZeros)
                 {
                     var endTime = DateTime.Now;
                     TimeSpan elapsedTimeInSeconds = endTime - startTime;
@@ -98,7 +100,7 @@ namespace BlockChainTutorial
                     
                     // winner winner chicken dinner nonce = 135819
 
-                    Assert.IsFalse(checkABC_For_0, mSeconds + " " + nonceValue);
+                    Assert.IsFalse(checkABC_For_leadingZeros, mSeconds + " " + nonceValue);
                 }
                 nonce++;
             }
@@ -109,9 +111,11 @@ namespace BlockChainTutorial
         public void ShouldTestTheRotor()
         {
             //int nonce = 0;
+            int difficulty = 3;
+            var leadingZeros = new string('0', difficulty);
             var startTime = DateTime.Now;
             Random random = new Random();
-            int nonce = random.Next(135800, 140000);
+            int nonce = random.Next(0, 900000000);
             SHA256 sHA256 = SHA256.Create();
             byte[] inputBytes_ABC = Encoding.ASCII.GetBytes("ABC");
 
@@ -122,27 +126,52 @@ namespace BlockChainTutorial
             string ABC_PlusNonce = Convert.ToBase64String(outputBytes_ABC_PlusNonce);
 
             // nonce should be 135819
-            bool check_ABC_PlusNonce_For_Leading_Zeros_000 = ABC_PlusNonce.StartsWith("000");
+            bool check_ABC_PlusNonce_For_leadingZeros = ABC_PlusNonce.StartsWith(leadingZeros);
 
-            while (!check_ABC_PlusNonce_For_Leading_Zeros_000)
+            while (!check_ABC_PlusNonce_For_leadingZeros)
             {
-                nonce = random.Next(135800, 140000);
+                nonce = random.Next(0, 900000000);
                 inputBytes_ABC_PlusNonce = Encoding.ASCII.GetBytes($"{inputBytes_ABC}-{nonce}");
                 outputBytes_ABC_PlusNonce = sHA256.ComputeHash(inputBytes_ABC_PlusNonce);
 
                 ABC_PlusNonce = Convert.ToBase64String(outputBytes_ABC_PlusNonce);
 
-                check_ABC_PlusNonce_For_Leading_Zeros_000 = ABC_PlusNonce.StartsWith("000");
+                check_ABC_PlusNonce_For_leadingZeros = ABC_PlusNonce.StartsWith(leadingZeros);
             }
 
-            if (check_ABC_PlusNonce_For_Leading_Zeros_000)
+            if (check_ABC_PlusNonce_For_leadingZeros)
             {
                 var endTime = DateTime.Now;
                 TimeSpan elapsedTimeInSeconds = endTime - startTime;
                 double mSeconds = elapsedTimeInSeconds.TotalSeconds;
                 string nonceValue = nonce.ToString();
-                Assert.IsFalse(check_ABC_PlusNonce_For_Leading_Zeros_000, mSeconds + " " + nonceValue);
+                Assert.IsFalse(check_ABC_PlusNonce_For_leadingZeros, mSeconds + " " + nonceValue);
             }
+        }
+
+        [TestMethod]
+        public void TestForSpecificNonce()
+        {
+            //int nonce = 277555444;
+            //int nonce = 865140105;
+            int nonce = 135819;
+            int difficulty = 3;
+            var leadingZeros = new string('0', difficulty);
+            SHA256 sHA256 = SHA256.Create();
+
+            byte[] inputBytes_ABC = Encoding.ASCII.GetBytes("ABC");
+            byte[] outputBytes_ABC = sHA256.ComputeHash(inputBytes_ABC);
+
+            string outputBytes_ABC_Hash = Convert.ToBase64String(outputBytes_ABC);
+
+            byte[] inputBytes_ABCPlusNonce = Encoding.ASCII.GetBytes($"{inputBytes_ABC}-{nonce}");
+            byte[] outputBytes_ABCPlusNonce = sHA256.ComputeHash(inputBytes_ABCPlusNonce);
+
+            string ABC_PlusNonce = Convert.ToBase64String(outputBytes_ABCPlusNonce);
+
+            bool check_ABC_PlusNonce_For_leadingZeros = ABC_PlusNonce.StartsWith(leadingZeros);
+
+            Assert.IsFalse(check_ABC_PlusNonce_For_leadingZeros);
         }
     }
 }
